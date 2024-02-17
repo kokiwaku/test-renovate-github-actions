84a8388cc21455d74ce0233080e0d52a9e571a3d
koki waku <kokiwaku1212@gmail.com>
2024-02-18 00:37:10 +0900

    [mod]一旦、packageRulesの設定を削除

--- renovate-config.js
+++ renovate-config.js
@@ -15,23 +15,5 @@ module.exports = {
     // 対象リポジトリを設定
     repositories: [
         'kokiwaku/test-renovate'
-    ],
-    // パッケージ毎の更新ルールを指定
-    packageRules: [
-        {
-            description: 'Dockerfile',
-            matchFileNames: ['docker/**/Dockerfile'],
-            // TODO 他のルール設定...
-        },
-        {
-            description: 'composer.json',
-            matchFileNames: ['src/composer.json'],
-            // TODO 他のルール設定...
-        },
-        {
-            description: 'package.json',
-            matchFileNames: ['src/package.json'],
-            // TODO 他のルール設定...
-        },
     ]
 };
